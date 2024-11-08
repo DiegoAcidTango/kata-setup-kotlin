a6540d24e9c8010cb80222479bc28742de6e87e5
DiegoAcidTango <diegoperez@acidtango.com>
2024-11-08 16:12:06 +0000

    Second test success - Implemented add method Third test fail - project method doesn't return products of the last days

--- src/main/kotlin/dev/kata/stringcalculator/Inventory.kt
+++ src/main/kotlin/dev/kata/stringcalculator/Inventory.kt
@@ -3,7 +3,13 @@ package dev.kata.stringcalculator
 import java.util.Date
 
 class Inventory {
+    private val products = mutableMapOf<Date, MutableList<String>>()
+
     fun project(date: Date) : List<String> {
-        return emptyList()
+        return products[date] ?: emptyList()
+    }
+
+    fun add(date: Date, item: String) {
+        products.computeIfAbsent(date) { mutableListOf() }.add(item)
     }
 }
--- src/test/kotlin/dev/kata/stringcalculator/StringCalculatorShould.kt
+++ src/test/kotlin/dev/kata/stringcalculator/StringCalculatorShould.kt
@@ -2,6 +2,9 @@ package dev.kata.stringcalculator
 
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.Date
 
 internal class StringCalculatorShould {
@@ -16,7 +19,7 @@ internal class StringCalculatorShould {
     }
 
     @Test
-    fun `Add item to inventory`() {
+    fun `Add product to inventory`() {
         val inventory = Inventory()
         val date = Date()
         val apple = "apple"
@@ -25,5 +28,49 @@ internal class StringCalculatorShould {
         val result = inventory.project(date)
 
         assertThat(result).isEqualTo(listOf("apple"))
+    }
+
+    @Test
+    fun `Given multiple products on the same date, it should return all products`() {
+        val inventory = Inventory()
+        val date = Date()
+        val apple = "apple"
+        val banana = "banana"
+
+        inventory.add(date, apple)
+        inventory.add(date, banana)
+        val result = inventory.project(date)
+
+        assertThat(result).isEqualTo(listOf("apple", "banana"))
+    }
+
+    @Test
+    fun `Given two same products on the same day, it should return two products`() {
+        val inventory = Inventory()
+        val date = Date()
+        val apple = "apple"
+
+        inventory.add(date, apple)
+        inventory.add(date, apple)
+        val result = inventory.project(date)
+
+        assertThat(result).isEqualTo(listOf("apple", "apple"))
+    }
+
+    @Test
+    fun `Given two products on different days, it should return past days products`() {
+        val inventory = Inventory()
+        val date1 = LocalDate.of(2021, 1, 1).atStartOfDay(ZoneId.systemDefault()).toInstant().let { Date.from(it) }
+        val date2 = LocalDate.of(2021, 1, 2).atStartOfDay(ZoneId.systemDefault()).toInstant().let { Date.from(it) }
+        val apple = "apple"
+        val banana = "banana"
+
+        inventory.add(date1, apple)
+        inventory.add(date2, banana)
+        val result1 = inventory.project(date1)
+        val result2 = inventory.project(date2)
+
+        assertThat(result1).isEqualTo(listOf("apple"))
+        assertThat(result2).isEqualTo(listOf("apple", "banana"))
     }
 }
